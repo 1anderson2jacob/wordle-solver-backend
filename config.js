6bb9e9a7f4de0679e029ebdffafa2cc25a7550ad
Jacob Anderson <1anderson2jacob@gmail.com>
2022-05-06 16:49:47 -0700

    made config.js ready for prod

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const { Pool } = require('pg');
 const connectionString =
   `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new Pool({
-  connectionString: connectionString,
+  // connectionString: connectionString,
   // connectionString: process.env.DATABASE_URL,
-  // connectionString: process.env.HEROKU_POSTGRESQL_BRONZE_URL,
+  connectionString: process.env.HEROKU_POSTGRESQL_BRONZE_URL,
 
-  ssl: false, // needs enabled for local
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  // ssl: false, // needs enabled for local
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = { pool };
